8fbeb9169881abb13f3fa0ece973ef7726ab3355
joshaber <joshaber@gmail.com>
2018-07-13 08:28:37 -0400

    Maybe this?

--- .next/server/bundles/pages/index.js
+++ .next/server/bundles/pages/index.js
@@ -87,7 +87,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
 
 /* harmony default export */ __webpack_exports__["default"] = (function () {
-  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", null, "Welcome to next.js!");
+  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", null, "Welcome to next.js! :)");
 });
 
 /***/ }),
