bfb08a3fcd8d9499f2950a78485b4c9fe863a878
joshaber <joshaber@gmail.com>
2018-07-13 08:30:52 -0400

    Build it

--- docs/_next/6e82be15-d27f-4e76-b48e-024488d75fb7/page/index.js
+++
@@ -1,1 +0,0 @@
-module.exports=__NEXT_REGISTER_PAGE("/",function(){return{page:webpackJsonp([2],{195:function(e,t,n){e.exports=n(196)},196:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(12),o=n.n(u);t.default=function(){return o.a.createElement("div",null,"Welcome to next.js! :)")}}},[195]).default}});
